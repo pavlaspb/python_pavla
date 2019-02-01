9b3ef146bd493fbfc101bf932e192b4de6b2c3df
pavlaspb <Github812!>
2019-02-01 09:18:58 +0300

    дз4

---
+++ application_contact.py
@@ -0,0 +1,64 @@
+from selenium import webdriver
+
+class Application_contact:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(60)
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_contacts_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
+
+    def create_new_contact(self, contact):
+        wd = self.wd
+        # init new contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill new contact form
+        self.contact_filling(contact)
+        # submit creation
+        wd.find_element_by_xpath("(//input[@name='submit'])").click()
+
+    def contact_filling(self, contact):
+        wd = self.wd
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+
+    def login(self, login, password):
+        wd = self.wd
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(login)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def destroy(self):
+        self.wd.quit()
